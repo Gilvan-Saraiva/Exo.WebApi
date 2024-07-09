49d497ccba6d5b32c06abeb0de2c3fc4bbceb1ea
Gilvan-Saraiva <juniorsaraivabi@gmail.com>
2024-07-09 12:32:59 -0300

    Versao 1

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,26 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+
+namespace Exo.WebApi.Contexts{
+    public class ExoContext : DbContext
+    {
+        public ExoContext(){
+
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options){
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured){
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;"
+                                            + "Database=ExoApi;Trusted_Connection=true;");
+            }
+       
+        }
+        public DbSet<Projeto> Projetos {get; set;}
+    }
+}
